ce8115e2e4eae0dcae531237ba283f2d6d935bbd
Solid Water Dynamic <85194566+Solid-Water-Dynamic@users.noreply.github.com>
2022-01-04 01:23:23 -0500

    first commit

---
+++ learnspring5webapp/src/main/java/net/azurewebsites/solidwater/learnspring5webapp/LearnSpring5WebAppApplication.java
@@ -0,0 +1,13 @@
+package net.azurewebsites.solidwater.learnspring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpring5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpring5WebAppApplication.class, args);
+	}
+
+}
---
+++ learnspring5webapp/src/test/java/net/azurewebsites/solidwater/learnspring5webapp/LearnSpring5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.azurewebsites.solidwater.learnspring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpring5WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
