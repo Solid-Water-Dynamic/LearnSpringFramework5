d64cfdf2ad963b32bf62ec4f3799565159593eaf
Solid Water Dynamic <85194566+Solid-Water-Dynamic@users.noreply.github.com>
2022-01-04 22:22:19 -0500

    incremental update

---
+++ learnspring5webapp/src/main/java/net/azurewebsites/solidwater/learnspring5webapp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package net.azurewebsites.solidwater.learnspring5webapp.controllers;
+
+import net.azurewebsites.solidwater.learnspring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String authors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
